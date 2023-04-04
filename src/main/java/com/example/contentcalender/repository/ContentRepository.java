c62e512605f92d1057286bfd13cec00c56eb35f8
DmitrySokoltsov <dmitrysokoltsov@gmail.com>
2023-04-04 22:01:01 +0300

    interface ContentRepository ListCrudRepository

---
+++ src/main/java/com/example/contentcalender/repository/ContentRepository.java
@@ -0,0 +1,10 @@
+package com.example.contentcalender.repository;
+
+import com.example.contentcalender.model.Content;
+import org.springframework.data.repository.ListCrudRepository;
+
+import java.util.List;
+
+public interface ContentRepository extends ListCrudRepository<Content,Integer> {
+    List<Content> findAllByTitleContains(String keyword);
+}
